470de4ca713ad9fa9ef03a57cd25e2a228a538a9
Nathan Cunha <nathan.cunha@TechMahindraRJ.com.br>
2019-01-02 15:24:09 -0200

    testando classe domino categoria

--- src/main/java/com/nathancunha/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nathancunha/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.nathancunha.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nathancunha.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Está Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1,"Informática");
+		Categoria categoria2 = new Categoria(2,"Escritorio");
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
+		
 	}
 
 }
