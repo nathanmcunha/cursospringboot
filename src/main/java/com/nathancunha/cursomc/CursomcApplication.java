b4f9094ae59962d19679ce6f22456b499cc473a1
Nathan Cunha <nathan.cunha@TechMahindraRJ.com.br>
2019-01-03 15:41:37 -0200

    Cliente ,tipo cliente ,telefones e enderecos

--- src/main/java/com/nathancunha/cursomc/CursomcApplication.java
+++ src/main/java/com/nathancunha/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nathancunha.cursomc.domain.Categoria;
 import com.nathancunha.cursomc.domain.Cidade;
+import com.nathancunha.cursomc.domain.Cliente;
+import com.nathancunha.cursomc.domain.Endereco;
 import com.nathancunha.cursomc.domain.Estado;
 import com.nathancunha.cursomc.domain.Produto;
+import com.nathancunha.cursomc.domain.enums.TipoCliente;
 import com.nathancunha.cursomc.repositories.CategoriaRepository;
 import com.nathancunha.cursomc.repositories.CidadeRepository;
+import com.nathancunha.cursomc.repositories.ClienteRepository;
+import com.nathancunha.cursomc.repositories.EnderecoRepository;
 import com.nathancunha.cursomc.repositories.EstadoRepository;
 import com.nathancunha.cursomc.repositories.ProdutoRepository;
 
@@ -30,20 +35,24 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		instanciaCateogoriaProduto();
 		
-		instanciaEstadoCidade();
-	}
-
-	private void instanciaEstadoCidade() {
+		
 		Estado estado1 = new Estado(null,"Minas Gerais");
 		Estado estado2 = new Estado(null,"São Paulo");
 		
@@ -56,9 +65,23 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
 		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2,cidade3));
+		
+		Cliente cli1 = new Cliente(null,"Maria Silva","maria@nogmail.com", "41084144034", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("37107433","981582550"));
+		
+		Endereco end1 = new Endereco(null, "Rua Xenofonte", "173", "ap 555", "Jardim Graziela", "86036-090", cli1 , cidade1);
+		
+		Endereco end2 = new Endereco(null, "Avenida Matos", "173", "ap 555", "Centro", "86036-080", cli1 , cidade2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(end1,end2));
+		
+		instanciaCategoriaProduto();
 	}
 
-	private void instanciaCateogoriaProduto() {
+	private void instanciaCategoriaProduto() {
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 
