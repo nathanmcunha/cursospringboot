12bbdc71082337b392305afeaf2a55137b3cdbca
Nathan Cunha <nathan.cunha@TechMahindraRJ.com.br>
2019-01-09 16:12:31 -0200

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/nathancunha/cursomc/CursomcApplication.java
+++ src/main/java/com/nathancunha/cursomc/CursomcApplication.java
@@ -45,75 +45,77 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
-	private EnderecoRepository enderecoRepository; 
-	
+	private EnderecoRepository enderecoRepository;
+
 	@Autowired
-	private PedidoRepository pedidoRepository; 
-	
+	private PedidoRepository pedidoRepository;
+
 	@Autowired
-	private PagamentoRepository pagamentoRepository;  
-	
+	private PagamentoRepository pagamentoRepository;
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
-	
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
-		Estado estado1 = new Estado(null,"Minas Gerais");
-		Estado estado2 = new Estado(null,"São Paulo");
-		
-		Cidade cidade1 = new Cidade(null,"Uberlândia",estado1);
-		Cidade cidade2 = new Cidade(null,"São Paulo",estado2);
-		Cidade cidade3 = new Cidade(null,"Campinas",estado2);
-		
+
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+
 		estado1.getCidades().addAll(Arrays.asList(cidade1));
 		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
-		
+
 		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
-		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2,cidade3));
-		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@nogmail.com", "41084144034", TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("37107433","981582550"));
-		
-		Endereco end1 = new Endereco(null, "Rua Xenofonte", "173", "ap 555", "Jardim Graziela", "86036-090", cli1 , cidade1);
-		
-		Endereco end2 = new Endereco(null, "Avenida Matos", "173", "ap 555", "Centro", "86036-080", cli1 , cidade2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
-		
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@nogmail.com", "41084144034", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("37107433", "981582550"));
+
+		Endereco end1 = new Endereco(null, "Rua Xenofonte", "173", "ap 555", "Jardim Graziela", "86036-090", cli1,
+				cidade1);
+
+		Endereco end2 = new Endereco(null, "Avenida Matos", "173", "ap 555", "Centro", "86036-080", cli1, cidade2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(end1,end2));
-		
-		Categoria categoria1 = new Categoria(null, "Informática");
-		Categoria categoria2 = new Categoria(null, "Escritório");
+		enderecoRepository.saveAll(Arrays.asList(end1, end2));
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 
 		Produto prod1 = new Produto(null, "Computador", 2000.00);
 		Produto prod2 = new Produto(null, "Impressora", 800.00);
 		Produto prod3 = new Produto(null, "Mouse", 80.00);
 
-		categoria1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
-		categoria2.getProdutos().addAll(Arrays.asList(prod2));
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
 
-		prod1.getCategorias().addAll(Arrays.asList(categoria1));
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
 
-		prod2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
-		prod3.getCategorias().addAll(Arrays.asList(categoria1));
-
-		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, end1);
@@ -122,15 +124,15 @@ public class CursomcApplication implements CommandLineRunner {
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
 
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
 		ped2.setPagamento(pagto2);
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
-		
-		
+
 		ItemPedido ip1 = new ItemPedido(ped1, prod1, 0.00, 1, 2000.00);
 		ItemPedido ip2 = new ItemPedido(ped1, prod3, 0.00, 2, 80.00);
 		ItemPedido ip3 = new ItemPedido(ped2, prod2, 100.00, 1, 800.00);
@@ -144,7 +146,5 @@ public class CursomcApplication implements CommandLineRunner {
 
 		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
-
-
 
 }
--- src/main/java/com/nathancunha/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nathancunha/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,8 +40,8 @@ public class CategoriaResource {
 		obj = categoriaService.insert(obj);
 		return ResponseEntity.created(buildURIResource(obj)).build();
 	}
-	
-	@RequestMapping(value ="/{id}",method = RequestMethod.PUT)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = categoriaService.update(obj);
@@ -50,7 +52,7 @@ public class CategoriaResource {
 		return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 	}
 
-	@RequestMapping(value ="/{id}",method = RequestMethod.DELETE)
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
@@ -58,7 +60,7 @@ public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
-		List <Categoria> categorias = categoriaService.findAll();
+		List<Categoria> categorias = categoriaService.findAll();
 
 		return ResponseEntity.ok().body(CategoriasDomainToDTO(categorias));
 
@@ -68,5 +70,16 @@ public class CategoriaResource {
 		return categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 	}
 
-	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Categoria> categorias = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> categoriasDTO = categorias.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(categoriasDTO);
+
+	}
+
 }
