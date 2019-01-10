784440c61442f0d4b1df98238cb5b710e0dd5c5b
Nathan Cunha <nathan.cunha@TechMahindraRJ.com.br>
2019-01-10 16:36:29 -0200

    personalizando o retorno da validacao

--- src/main/java/com/nathancunha/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nathancunha/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.nathancunha.cursomc.resources.exception;
 
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -20,13 +21,26 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrityException(DataIntegrityException e, HttpServletRequest request) {
 
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validacao",System.currentTimeMillis());
+		
+		//TODO: MUDAR PARA LAMBDA/STREAM
+		for (FieldError erro : e.getBindingResult().getFieldErrors()) {
+			err.addError(erro.getField(), erro.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
 
 }
